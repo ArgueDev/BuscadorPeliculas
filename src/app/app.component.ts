a0f8e59b40eb4767685313bf5cf50b71d14521a3
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-02-25 09:13:54 -0500

    backdrop_path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pagina/home/home.component'; // Import the correct component
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true, // Remove incorrect imports
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'BuscadorSpotify';
--- src/app/pagina/home/home.component.ts
+++ src/app/pagina/home/home.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BuscadorPeliculasService } from '../../Services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  bannnerApi: any = []; // Declare the 'bannnerApi' property
 
+  constructor(private api: BuscadorPeliculasService) { }
+
+  ngOnInit() {
+    this.bannerData();
+  }      
+
+  bannerData() {
+    this.api.bannerApiData().subscribe((result) => {
+      console.log(result, 'bannerresult#');
+      this.bannnerApi = result.results;
+    });
+  }
 }
