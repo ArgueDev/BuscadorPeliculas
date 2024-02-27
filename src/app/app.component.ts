8bda37cdbe762b26c38a22265581c598135633ae
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-02-27 18:42:01 -0500

    barra de navegación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pagina/home/home.component'; // Import the correct component
+import { HomeComponent } from './pagina/home/home.component';
+import { NavigationComponent } from "./pagina/navigation/navigation.component"; // Import the correct component
 
 @Component({
     selector: 'app-root',
     standalone: true, // Remove incorrect imports
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent]
+    imports: [RouterOutlet, HomeComponent, NavigationComponent]
 })
 export class AppComponent {
   title = 'BuscadorSpotify';
--- src/app/pagina/home/home.component.ts
+++ src/app/pagina/home/home.component.ts
@@ -1,27 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BuscadorPeliculasService } from '../../Services/api.service';
 import { CommonModule } from '@angular/common';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule], // Add NgbCarouselModule to the imports array
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
+
 })
 export class HomeComponent implements OnInit{
+
   bannnerApi: any = []; // Declare the 'bannnerApi' property
+  timerSubscription: any;
 
   constructor(private api: BuscadorPeliculasService) { }
+  currentIndex = 0;
+  interval = 5000;
 
   ngOnInit() {
     this.bannerData();
-  }      
+    this.startAutoPlay();
+  }   
+
+  ngOnDestroy() {
+    // Cancelar la suscripción al destruir el componente para evitar fugas de memoria
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  stopAutoPlay() {
+    // Detiene el autoplay cancelando la suscripción al temporizador
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
 
   bannerData() {
     this.api.bannerApiData().subscribe((result) => {
       console.log(result, 'bannerresult#');
       this.bannnerApi = result.results;
+    });
+  }
+
+
+nextImage() {
+  this.currentIndex = (this.currentIndex + 1) % this.bannnerApi.length;
+}
+
+prevImage() {
+  this.currentIndex = (this.currentIndex - 1 + this.bannnerApi.length) % this.bannnerApi.length;
+}
+
+truncatedOverview(overview: string, index: number): string {
+  const maxLength = 100; // Longitud máxima del texto a mostrar inicialmente
+  return this.bannnerApi[index].showFullOverview ? overview : (overview.length > maxLength ? overview.substring(0, maxLength) + '...' : overview);
+}
+
+toggleOverview(index: number) {
+  this.bannnerApi[index].showFullOverview = !this.bannnerApi[index].showFullOverview;
+}
+
+isOverMaxLength(overview: string, index: number): boolean {
+  const maxLength = 100; // Longitud máxima del texto a mostrar inicialmente
+  return overview.length > maxLength && !this.bannnerApi[index].showFullOverview;
+}
+
+ startAutoPlay() {
+    // Inicia el autoplay configurando un temporizador para llamar a nextImage() cada 'interval' milisegundos
+    this.timerSubscription = timer(0, this.interval).subscribe(() => {
+      this.nextImage();
     });
   }
 }
